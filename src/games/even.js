81de5b89836fcee6da2209cba120d24300503744
Nikita-imba <jvaleska896@mail.com>
2025-09-15 12:41:18 +0300

    Fix even game description with correct quotes

--- src/games/even.js
+++ src/games/even.js
@@ -6,22 +6,23 @@ export const runEvenGame = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log("Answer \'yes'\ if the number is even, otherwise answer 'no'.");
-  
+  console.log("Answer \"yes\" if the number is even, otherwise answer \"no\"."); // Исправлено
+
   for (let i = 0; i < 3; i += 1) {
     const number = Math.floor(Math.random() * 100) + 1;
     console.log(`Question: ${number}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = isEven(number) ? 'yes' : 'no';
-    
+
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
-    
+
     console.log('Correct!');
   }
-  
+
   console.log(`Congratulations, ${name}!`);
 };
+
